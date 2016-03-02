29684096ffb5e2b6871e19ce3d67dcdf27365e81
Daniel Hu <hdc@pantum.com>
2016-03-02 18:32:13 +0800

    填写周报 OK

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,21 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var ReviewModel = require('../models/db').model('Review');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-//  var date = new Date();
-//  date.getFullYear();
-  res.render('reviews/index', {title: 'Job reviews',
-                               years:[2016,2017,2018],
-                               months: [1,2,3,4,5,6,7,8,9,10,11,12],
-                               reviews: [{catalog:'项目维护',task:'伟思安全机',content:'调用DLL',progress:'完成',cost:8}]
-                              });
+  //  var date = new Date();
+  //  date.getFullYear();
+  ReviewModel.find(function(err, reviews) {
+    res.render('reviews/index', {title: 'Job reviews',
+                                 years:[2016,2017,2018],
+                                 months: [1,2,3,4,5,6,7,8,9,10,11,12],
+                                 reviews: reviews
+                                });
+  })
 
 });
 
 
-router.post('/', function(req, res, next) {
-
+router.post('/new', function(req, res, next) {
+  var review = ReviewModel(req.body);
+  review.save();
+  res.json({r:'OK'});
 })
 
 
