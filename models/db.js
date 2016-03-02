757b864d9f36bf02729ee71f6ee5e5940a9d5377
Daniel Hu <hdc@pantum.com>
2016-03-02 14:58:35 +0800

    js操作table

--- models/db.js
+++ models/db.js
@@ -11,11 +11,29 @@ var db = mongoose.connect('mongodb://localhost/job-review');
 var Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
-var Review = new Schema({
+var TaskSchema = new Schema({
   author : ObjectId,
-  title  : String,
-  body   : String,
-  date   : Date
+  parent : ObjectId,
+  catalog: ObjectId,
+})
+
+var ReviewSchema = new Schema({
+  author  : ObjectId,
+  isoWeek : Number, // 周数
+  catalog : String, // 工作类型
+  task    : String, // 工作任务
+  content : String, // 工作内容
+  progress : String, // 完成情况
+  cost : Number, // 用时
+  created : Date,
+  modified : Date,
+  checked : Date
+})
+
+var CommentSchema = new Schema({
+  author : ObjectId,
+  review : ObjectId,
+  content : String
 })
 
 var UserSchema = new Schema({
@@ -27,6 +45,7 @@ var UserSchema = new Schema({
 //  date      : Date
 })
 
+db.model('Review', ReviewSchema);
 db.model('User', UserSchema);
 
 var BlogPost = new Schema({
--- routes/reviews.js
+++ routes/reviews.js
@@ -5,9 +5,18 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 //  var date = new Date();
 //  date.getFullYear();
-  res.render('reviews/index', {title: 'Job reviews', years:[2016,2017,2018], months: [1,2,3,4,5,6,7,8,9,10,11,12]});
+  res.render('reviews/index', {title: 'Job reviews',
+                               years:[2016,2017,2018],
+                               months: [1,2,3,4,5,6,7,8,9,10,11,12],
+                               reviews: [{catalog:'项目维护',task:'伟思安全机',content:'调用DLL',progress:'完成',cost:8}]
+                              });
 
 });
+
+
+router.post('/', function(req, res, next) {
+
+})
 
 
 router.get('/new', function(req, res, next) {
