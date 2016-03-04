f064381133af5b4639fc5aef9c44fe43fb4f8088
Daniel Hu <hdc@pantum.com>
2016-03-04 11:04:20 +0800

    使用 ES6 模版字符串

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+//console.log(app);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
 var ReviewModel = require('../models/db').model('Review');
+var hbs = require('hbs');
+
+hbs.registerHelper('', function() {
+
+});
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
